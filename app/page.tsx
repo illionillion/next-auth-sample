17467c172fdeac3ba7cfa5a0626633f4d68e1a92
yuto ichinose <yuto.0526.sano@gmail.com>
2024-08-03 00:39:01 +0900

    修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,36 @@
 import { Button, Center, Container, Text } from '@yamada-ui/react';
-// import { signIn } from 'next-auth/react';
 import { auth, signIn, signOut } from '@/auth'
 
 export default async function Home() {
 
   const session = await auth();
-  if (session) {
-    return (
-      <Container as={Center} m="auto" h="100dvh">
-        <Text>Home Page</Text>
-        <Text>Welcome, {session.user?.name}!</Text>
-          <form
-            action={async () => {
-              "use server";
-              await signOut();
-            }}
-          >
-            <Button type="submit">ログアウト</Button>
-          </form>
-      </Container>
-    );
-  }
+  const name = session?.user?.name
 
   return (
     <Container as={Center} m="auto" h="100dvh">
-      <form
-        action={async () => {
-          "use server";
-          await signIn("github");
-        }}
-      >
-        <Button type="submit">ログイン</Button>
-      </form>
+      {
+        session ?
+          <>
+            <Text>Home Page</Text>
+            <Text>Welcome, {name}!</Text>
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <Button type="submit">ログアウト</Button>
+            </form>
+          </> :
+          <form
+            action={async () => {
+              "use server";
+              await signIn("github");
+            }}
+          >
+            <Button type="submit">ログイン</Button>
+          </form>
+      }
     </Container>
   );
 }
